bb0acb5d96faeb242e534c9ecf789834f207df8e
m4paulo <matheuspaulomp456@gmail.com>
2026-05-08 12:53:04 -0300

    feat: estrutura inicial do projeto CleanTaggy

---
+++ Back-end/core/settings.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+BASE_DIR = path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-chave-provisoria'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'api',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'  
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }   
+}
+
+STATIC_URL = '/static/'
