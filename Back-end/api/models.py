a6b90d9f9453102490991fb3364dc88a8992c93c
kaio cerqueira <kaiocerqueira14@gmail.com>
2026-05-09 01:14:59 -0300

    feat: cria modelo de transação para persistência close #2

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -1,1 +1,12 @@
- 
+from django.db import models
+
+class Transacao(models.Model):
+    local = models.CharField(max_length=255)
+    valor_pedagio = models.DecimalField(max_digits=10, decimal_places=2)
+    co2_economizado = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return f"{self.local} - {self.data}"
+
+
