f81838e2f421b07003cf6618528bb17984de5e0c
CCZer0 <123778644+CCZer0@users.noreply.github.com>
2026-05-14 10:53:02 -0300

    Update urls.py
    git commit -m "feat: configura rotas de navegação para o ranking close #5"

--- Back-end/core/urls.py
+++ Back-end/core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from api import views # Importa todas as suas views de uma vez
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', views.dashboard, name='dashboard'),
+    path('history/', views.history, name='history'),
+    path('sustainability/', views.sustainability, name='sustainability'),
+    path('community/', views.community, name='community'),
+    path('profile/', views.profile, name='profile'),
 ]
