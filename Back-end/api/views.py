e0a32606d5f648a9bcafdcfa521c9e9df647deed
ArthurGuimaraes <arthurvictorguimaraes2006@gmail.com>
2026-05-13 23:29:56 -0300

    feat: implementa lógica de cálculo de CO2 no dashboard close #3

--- Back-end/api/views.py
+++ Back-end/api/views.py
@@ -1,1 +1,23 @@
- 
+from django.http import JsonResponse
+from .models import Transacao
+
+def dashboard(request):
+    transacoes = Transacao.objects.all()
+
+    total_co2 = sum(
+        transacao.co2_economizado
+        for transacao in transacoes
+    )
+
+    total_pedagio = sum(
+        transacao.valor_pedagio
+        for transacao in transacoes
+    )
+
+    data = {
+        "total_co2_economizado": float(total_co2),
+        "total_pedagios": float(total_pedagio),
+        "quantidade_transacoes": transacoes.count(),
+    }
+
+    return JsonResponse(data)
