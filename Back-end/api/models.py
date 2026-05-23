603fc927dea7f799ea4272ecdd4d3f1ab5177ac1
JoaoVitorDev <Joaolopes7193@gmail.com>
2026-05-22 23:16:35 -0300

    Atualizacao de models.py

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Transacao(models.Model):
-    local = models.CharField(max_length=255)
-    valor_pedagio = models.DecimalField(max_digits=10, decimal_places=2)
-    co2_economizado = models.DecimalField(max_digits=10, decimal_places=2)
-    data = models.DateTimeField(auto_now_add=True)
+class Veiculo(models.Model):
+    COMBUSTIVEL_CHOICES = [
+        ('GASOLINA', 'Gasolina'),
+        ('DIESEL', 'Diesel'),
+        ('ETANOL', 'Etanol'),
+    ]
+    CATEGORIA_CHOICES = [
+        ('HATCH', 'Hatch Compacto (Leve)'),
+        ('SEDAN', 'Sedan / SUV (Médio)'),
+        ('UTILITARIO', 'Picape / Utilitário (Pesado)'),
+    ]
 
-def __str__(self):
-    return f"{self.local} - {self.data}"
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    modelo = models.CharField(max_length=100, null=True, blank=True)
+    placa = models.CharField(max_length=20, null=True, blank=True)
+    tipo_combustivel = models.CharField(max_length=50, choices=COMBUSTIVEL_CHOICES)
+    categoria = models.CharField(max_length=50, choices=CATEGORIA_CHOICES)
+    rendimento_exato = models.FloatField(null=True, blank=True)
 
-
+    def str(self):
+        return f"{self.get_categoria_display()} ({self.get_tipo_combustivel_display()})"
