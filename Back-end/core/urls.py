ab086b5d3282735e3fe13bd653bbe9150b57c975
Mateus Xavier <114512507+mateusxavierr@users.noreply.github.com>
2026-05-22 23:20:17 -0300

    Add logout path using Django auth views

--- Back-end/core/urls.py
+++ Back-end/core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from api import views # Importa todas as suas views de uma vez
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('sustainability/', views.sustainability, name='sustainability'),
     path('community/', views.community, name='community'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='dashboard'), name='logout'),
 ]
